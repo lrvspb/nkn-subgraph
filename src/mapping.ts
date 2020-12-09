1d1327c0cd51d6bad9d0d7fcc4e870359c8b8cf3
John Doe <johndoe@example.com>
2020-12-10 00:21:11 +0300

    Changing the name of an entity to ownerships

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,31 +5,31 @@ import {
   Transfer,
   Approval
 } from "../generated/NKN/NKN"
-import { ExampleEntity } from "../generated/schema"
+import { Ownership } from "../generated/schema"
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let ownership = Ownership.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (ownership == null) {
+    ownership = new Ownership(event.transaction.from.toHex())
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    ownership.count = BigInt.fromI32(0)
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  ownership.count = ownership.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
-  entity._from = event.params._from
-  entity._to = event.params._to
+  ownership._from = event.params._from
+  ownership._to = event.params._to
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  ownership.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
